00d20853efa22fa5fe27fe9cc9195a2ec34f6f62
Ross Jones <ross@servercode.co.uk>
2015-10-20 20:34:33 +0100

    Allow a disabled state

--- web/views/harvester_view.ex
+++ web/views/harvester_view.ex
@@ -4,6 +4,7 @@ defmodule Harvest.HarvesterView do
   def level_for_state("pending"), do: "info"
   def level_for_state("running"), do: "warning"
   def level_for_state("failed"), do: "danger"
+  def level_for_state("disabled"), do: "default"
   def level_for_state(_), do: "info"
 
 end
